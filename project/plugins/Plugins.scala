cf6aa6a33434ff1710256e41d604a23dde0c272d
Andreas Schmid <andreas.schmid@gmx.de>
2011-01-24 09:25:29 +0100

    changed to consistent naming of plugin

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -8,5 +8,5 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   lazy val sbtIdeaRepository = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
   lazy val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.2.0"
 
-  lazy val sbtTestNotifier = "de.aaschmid.sbtplugin" % "sbt-test-notify" % "0.1" //withSources()
+  lazy val sbtTestNotifier = "de.aaschmid.sbtplugin" % "sbt-test-notifier" % "0.1" //withSources()
 }
